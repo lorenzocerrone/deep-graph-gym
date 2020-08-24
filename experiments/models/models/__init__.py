ec5a6a74587982227e9be3b9117ad9179ed2894d
lorenzocerrone <lorenzo.cerrone@gmail.com>
2020-08-24 11:37:47 +0200

    add graph unet

--- experiments/models/models/__init__.py
+++ experiments/models/models/__init__.py
@@ -1,3 +1,3 @@
-from deepgraphgym.models.models.gcn import GCN
-
-models_collection = {"GCN": GCN}
+from experiments.models.models.gcn import GCN
+from experiments.models.models.graphunet import GraphUNet
+models_collection = {"GCN": GCN, "GraphUNet": GraphUNet}
---
+++ experiments/models/models/graphunet.py
@@ -0,0 +1,21 @@
+from torch_geometric.nn import GraphUNet as _GraphUNet
+from torch_geometric.utils import dropout_adj
+
+
+class GraphUNet(torch.nn.Module):
+    def __init__(self):
+        super(GraphUNet, self).__init__(config)
+        pool_ratios = [2000 / data.num_nodes, 0.5]
+        self.unet = _GraphUNet(config['num_input_features'], config['num_filters'], config['num_output_features'],
+                              depth=3, pool_ratios=pool_ratios)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        edge_index, _ = dropout_adj(edge_index, p=0.2,
+                                    force_undirected=True,
+                                    num_nodes=data.num_nodes,
+                                    training=self.training)
+        x = F.dropout(x, p=0.92, training=self.training)
+
+        x = self.unet(x, edge_index)
+        return F.log_softmax(x, dim=1)
